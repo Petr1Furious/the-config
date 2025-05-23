952cd896e9c75fed4268f58a565740783b554366
Petr Tsopa <petrtsopa03@gmail.com>
2025-05-23 16:04:31 +0300

    fix nginx redirects

--- nginx.nix
+++ nginx.nix
@@ -18,14 +18,20 @@
 
     services.nginx = {
       enable = true;
-      recommendedProxySettings = true;
+      recommendedGzipSettings = true;
       recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
       defaultListen = [
         {
           addr = "127.0.0.1";
           port = config.setup.nginxPort;
         }
       ];
+      commonHttpConfig = ''
+        port_in_redirect off;
+        absolute_redirect off;
+      '';
     };
   };
 }
