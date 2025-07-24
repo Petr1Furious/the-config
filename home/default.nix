ee60ec3421865fa42a74596c7cb5f70be6ef650d
Petr Tsopa <petrtsopa03@gmail.com>
2025-07-24 23:48:27 +0300

    add p10k

--- home/default.nix
+++ home/default.nix
@@ -30,6 +30,16 @@
 
     plugins = [
       {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k;
+        file = "p10k.zsh";
+      }
+      {
         name = "zsh-peco-history";
         src = pkgs.fetchFromGitHub {
           owner = "jimeh";
