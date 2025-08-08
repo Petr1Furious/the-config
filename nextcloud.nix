29cdb789717ecc2e2328f3af6aab1fd97645590a
Petr Tsopa <petrtsopa03@gmail.com>
2025-08-08 21:22:56 +0300

    fix nextcloud not redirecting after login

--- nextcloud.nix
+++ nextcloud.nix
@@ -34,19 +34,26 @@ in
       dbtype = "pgsql";
     };
 
-    settings.enabledPreviewProviders = [
-      "OC\\Preview\\BMP"
-      "OC\\Preview\\GIF"
-      "OC\\Preview\\JPEG"
-      "OC\\Preview\\Krita"
-      "OC\\Preview\\MarkDown"
-      "OC\\Preview\\MP3"
-      "OC\\Preview\\OpenDocument"
-      "OC\\Preview\\PNG"
-      "OC\\Preview\\TXT"
-      "OC\\Preview\\XBitmap"
-      "OC\\Preview\\HEIC"
-    ];
+    settings = {
+      trusted_proxies = [
+        "127.0.0.1"
+        "::1"
+      ];
+      settings.overwriteProtocol = "https";
+      enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
+    };
   };
 
   traefik.proxies = [
