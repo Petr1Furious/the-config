b81667efa964365ca8cb8c8d6944c01f2bc0ca25
Petr Tsopa <petrtsopa03@gmail.com>
2026-08-08 21:44:59 +0200

    rotate ssh keys

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -36,7 +36,9 @@
     ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDQ/qpe93Q2eJdJxsJDA5oKvsfWPFDlzsPaqdWf5Vg/q3yvxwlBRmL5Vlm5qbHlhOka380L+EYtTOspdeiprmPYxYF4B/3TYByc8ehnVON2emUsB6gZ6K4NC/WGx3jPQIiXQ7/OlpG0NafzzTN9RgVL2WyImImNolgThRyCblv+UpL2xd6wRML1C0/eME/j/ZxOObnd9RyMkWQI0OiJLITC0Q0xuLCMSNChgNFPhkS9yTC8HX8+2vgfgKcPYZ9s5wVMHg25MIM2/K6OeAYlFgom06hspiAxRjWlT3tDHzixJc649f8ioiv8FcOJ7uhae7USqDAgFJ5z+PbbodSdZFR/s6ARfufq7hd1lAkVrqz0nOqkM/n26NVW607w8XrrKscfJh255napd1wqnPCL9saTr4wsyNzGintqr+ciX/UdqcQ9R9Fuqh77/m0TZYYW6taRkElaLOVGhNMIbtk5DjLgymwlKDx20VYTrbCZbQGmcP2lzjCnziJGMUYO2vCyqwFPCM2yA+B3oOM1dvjD5+g4jguQSPVWNbV22nGdc9HxvWKGk+ACjQMJzMoOwjQOj9CWSNcsm1T1QScmaK1yQVjqrOS/nS2SqdG+aM14FOtilsiBbe+Rs6h3MvGocE6oi2lmkEY3JsuU6LOXz9Ew35A1fnkB9RwMxiAtwk375C3JBw== petrtsopa03@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYb7/Z36wmmKSdZ9RCvMtyb2LB5RATwNJFwftJ56VFz personal-macbook-2026-08"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAwFnjh88nmFf8hUcE20aYhkul2RN6gghrZUVJ1hxwoa second-macbook-2026-08"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGBPXCsXxvBzf7idsv8VhUwKc9+GM5vsGJ78HhjGxLyX phone-2026-08"
     ];
   };
 
