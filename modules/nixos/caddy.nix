0921349634766939b2ee0b07ba22c1451cd4f030
Petr Tsopa <petrtsopa03@gmail.com>
2026-08-22 14:16:28 +0200

    fix caddy hash

--- modules/nixos/caddy.nix
+++ modules/nixos/caddy.nix
@@ -53,7 +53,7 @@
         enableReload = true;
         package = pkgs.caddy.withPlugins {
           plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-          hash = "sha256-7GoH8YLCoPmPExQxoga2FHB58zQDoZVf1BBwkVi0SsQ=";
+          hash = "sha256-PWadA5qr/gR2qDcT8l8u1Xku7LM2HIfWTLOkzezCYy0=";
         };
         globalConfig = ''
           grace_period 30s
