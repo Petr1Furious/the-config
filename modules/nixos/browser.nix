b7393acf94bfa4e08f11e36902c86677c348d96f
Petr Tsopa <petrtsopa03@gmail.com>
2026-09-26 01:27:12 +0200

    route browser through tailscale exit node

--- modules/nixos/browser.nix
+++ modules/nixos/browser.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   pkgs-unstable,
   ...
@@ -9,12 +10,40 @@ let
   vncPort = 5901;
   cdpPort = 9222;
   webPort = 6080;
+  socksPort = 1055;
   width = 1600;
   height = 1000;
+  exitNode = "100.67.147.81";
+  tailscale = config.services.tailscale.package;
+  tsSocket = "/run/tailscale-browser/tailscaled.sock";
 in
 {
+  systemd.services.tailscaled-browser = {
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    serviceConfig = {
+      DynamicUser = true;
+      StateDirectory = "tailscale-browser";
+      RuntimeDirectory = "tailscale-browser";
+      LoadCredential = "authkey:${config.age.secrets.tailscale-authkey.path}";
+      ExecStart = "${tailscale}/bin/tailscaled --tun=userspace-networking --socks5-server=127.0.0.1:${toString socksPort} --statedir=/var/lib/tailscale-browser --socket=${tsSocket} --port=0";
+      ExecStartPost = pkgs.writeShellScript "tailscale-browser-up" ''
+        while [ ! -S ${tsSocket} ]; do sleep 0.2; done
+        ${tailscale}/bin/tailscale --socket=${tsSocket} up \
+          --auth-key=file:$CREDENTIALS_DIRECTORY/authkey \
+          --hostname=potato-browser \
+          --advertise-tags=tag:server \
+          --exit-node=${exitNode}
+      '';
+      Restart = "on-failure";
+    };
+  };
+
   systemd.services.browser = {
     wantedBy = [ "multi-user.target" ];
+    wants = [ "tailscaled-browser.service" ];
+    after = [ "tailscaled-browser.service" ];
     environment = {
       DISPLAY = ":${toString displayNum}";
       TZ = "Europe/Copenhagen";
@@ -27,6 +56,7 @@ in
       exec ${pkgs.chromium}/bin/chromium \
         --user-data-dir=/home/petrtsopa/browser/chrome \
         --remote-debugging-port=${toString cdpPort} \
+        --proxy-server=socks5://127.0.0.1:${toString socksPort} \
         --no-first-run \
         --window-position=0,0 \
         --window-size=${toString width},${toString height}
@@ -46,6 +76,11 @@ in
       ExecStart = "${pkgs.python3Packages.websockify}/bin/websockify --web ${pkgs.novnc}/share/webapps/novnc ${toString webPort} 127.0.0.1:${toString vncPort}";
       Restart = "on-failure";
     };
+  };
+
+  programs.chromium = {
+    enable = true;
+    extraOpts.WebRtcIPHandling = "disable_non_proxied_udp";
   };
 
   networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ webPort ];
